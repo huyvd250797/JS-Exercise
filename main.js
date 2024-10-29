ac259bb82015f10d025df4e0da559c4f730c0e6b
Huy Vo Duc <HuyVD@ASC.LOCAL>
2024-10-29 17:32:35 +0700

    13. Roman to Integer

--- main.js
+++ main.js
@@ -18,6 +18,37 @@ const isPrime = (num) => {
   return primeNum;
 };
 
+//TODO ------------------------------- 1. TWO SUM ------------------------------- */
+// Example 1: Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+const twoSum = () => {
+  // Khai báo mảng chứa các number
+  let numArr = [2, 7, 11, 15];
+
+  // Khai báo target: tổng của 2 số bất kỳ
+  let target = 9;
+
+  // Khai báo biến tổng 2 number
+  let sumTwoNumbers;
+
+  // Khai báo mảng rỗng chứa kết quả
+  let result = [];
+
+  for (let i = 0; i < numArr.length; i++) {
+    for (let j = numArr.length - 1; j > i; j--) {
+      // Gán tổng 2 number vào sumTwoNumbers
+      sumTwoNumbers = numArr[i] + numArr[j];
+
+      //   Nếu tổng 2 number = target thì push vào mảng
+      if (sumTwoNumbers == target) {
+        result.push(i, j);
+      }
+    }
+  }
+  console.log(result);
+};
+
 //TODO ---------------------------- 9. Palindrome Number : số đảo ngược --------------------------- */
 // Is Palinddrome Function
 const isPalindrome = (num) => {
@@ -57,3 +88,103 @@ const checkPalindrome = () => {
 
   console.log(isPalindrome(inputNumber));
 };
+
+//TODO -------------------------- 13. Roman to Integer -------------------------- */
+
+// Code writing
+const romanToInt = (roman) => {
+  // Number cần đổi
+  let romanNum = roman;
+
+  // Khai báo list int Number
+  let listNumber = [1, 5, 10, 50, 100, 500, 1000];
+
+  // Khai báo list roman Number
+  let listRoman = ["I", "V", "X", "L", "C", "D", "M"];
+
+  // Khai báo biến kết quả
+  let result = [];
+
+  // Khai báo mảng và tách các ký tự thành mảng
+  let romanLetter = romanNum.split("");
+
+  // Khai báo mảng rỗng để chứa number được chuyển từ roman
+  let transferNumber = [];
+
+  for (let i = 0; i < romanLetter.length; i++) {
+    for (let j = listRoman.length - 1; j >= 0; j--) {
+      // Nếu ký tự của chuỗi nhập vào == ký tự trong danh sách roman
+      if (romanLetter[i] == listRoman[j]) {
+        // push int number tương ứng với roman number vào mảng transfer
+        transferNumber.push(listNumber[j]);
+      }
+    }
+  }
+
+  // Kiểm tra các số ở vị trí thứ 4 và thứ 9
+  // ví dụ:IV = 4, IX = 9, CM = 900, XC = 90
+  for (let i = 0; i < transferNumber.length; i++) {
+    // Nếu giá trị thứ i < giá trị thứ i+1 --> là số vị trí thứ 4 hoặc 9
+    if (transferNumber[i] < transferNumber[i + 1]) {
+      // Lấy giá trị sau - giá trị trước
+      let number4th_9th = transferNumber[i + 1] - transferNumber[i];
+      result.push(number4th_9th);
+
+      // sau khi trừ bỏ qua 1 vị trí vì đã xét đến 2 ký tự
+      // Ví dụ 10, 100 ==> 90 thì bỏ qua ko xét đến phần từ 100 sau phần tử 10
+      i++;
+    } else {
+      result.push(transferNumber[i]);
+    }
+  }
+
+  // Number kết quả
+  let resultNumber = 0;
+  for (let i = 0; i < result.length; i++) {
+    // sum tất cả giá trị phần tử
+    resultNumber += result[i];
+  }
+  return resultNumber;
+};
+
+//!! CODE khác xử lý nhanh ngắn hơn
+var romanToIntSHORT = function (roman) {
+  // Khai báo đối tượng định nghĩa ký tự roman và int tương ứng
+  const romanNumber = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  // Khai báo biến kết quả
+  var result = 0;
+  for (var i = 0; i < roman.length; i++) {
+    // Kiểm tra các số ở vị trí thứ 4 và thứ 9
+    // ví dụ:IV = 4, IX = 9, CM = 900, XC = 90
+    // Nếu giá trị thứ i < giá trị thứ i+1 --> là số vị trí thứ 4 hoặc 9
+    if (romanNumber[roman[i]] < romanNumber[roman[i + 1]]) {
+      result = result + romanNumber[roman[i + 1]] - romanNumber[roman[i]];
+
+      // sau khi trừ bỏ qua 1 vị trí vì đã xét đến 2 ký tự
+      // Ví dụ 10, 100 ==> thì là số 90, bỏ qua ko xét đến phần từ 100 sau phần tử 10
+      i++;
+    } else {
+      result = result + romanNumber[roman[i]];
+    }
+  }
+  return result;
+};
+
+// Hàm chuyển ký tự roman sang number
+const transferRomanToIn = () => {
+  let romanNumber = document.getElementById("romanNumber").value;
+
+  document.getElementById("resultTransferRoman").innerHTML =
+    romanToIntSHORT(romanNumber);
+};
+
+//TODO ------------------------ 14. Longest Common Prefix: Kiểm tra vần chung ----------------------- */
